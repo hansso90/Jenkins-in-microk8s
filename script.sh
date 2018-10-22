a9dd4a8e43db02377fbc2632bda8773491f35b8d
Hans <hanslemkes@gmail.com>
2018-10-22 16:24:31 +0200

    jenkins-slave issue continue at home

--- script.sh
+++ script.sh
@@ -5,6 +5,9 @@ microk8s.kubectl proxy &
 
 microk8s.enable registry
 
+echo 'Take a 15 sec for the registery!'
+sleep 15
+
 microk8s.docker build -t localhost:32000/jenkins:1.0 jenkins
 microk8s.docker push localhost:32000/jenkins:1.0
 
@@ -14,3 +17,8 @@ microk8s.docker push localhost:32000/jenkins-slave:1.0
 microk8s.kubectl apply -f ./jenkins/jenkins-serviceaccount.yaml
 microk8s.kubectl apply -f ./jenkins/jenkins-config.yaml
 microk8s.kubectl apply -f ./jenkins/jenkins-deployment.yaml
+
+echo 'Sleep for 15 seconds the pod can start'
+sleep 15
+
+microk8s.kubectl port-forward svc/jenkins 9000:9000
